2b99901ef514d05e8cae25252d903651f9e9c143
Leeran <leeran7@gmail.com>
2022-07-18 18:11:24 -0400

    added hits with categories filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import algoliasearch from "algoliasearch/lite";
+import {
+	InstantSearch,
+	useHits,
+	useRefinementList,
+	UseRefinementListProps,
+} from "react-instantsearch-hooks-web";
+
+const searchClient = algoliasearch(
+	process.env.ALGOLIA_APP_ID ?? "",
+	process.env.ALGOLIA_API_KEY ?? ""
+);
 
 const Home: NextPage = () => {
 	return (
@@ -12,9 +23,49 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main className={styles.main}></main>
+			<main className={styles.main}>
+				<Main />
+			</main>
 		</div>
 	);
 };
 
+const Main: React.FC = () => {
+	return (
+		<InstantSearch
+			indexName="algolia-test-routing-bug"
+			searchClient={searchClient}
+			routing
+		>
+			<Filter attribute="categories" limit={20} />
+			<Hits />
+		</InstantSearch>
+	);
+};
+
+const Filter: React.FC<UseRefinementListProps> = (props) => {
+	const { items, refine } = useRefinementList(props);
+	if (items.length <= 1) return null;
+	return (
+		<div>
+			{items.map((i) => (
+				<button onClick={() => refine(i.value)}>{i.label}</button>
+			))}
+		</div>
+	);
+};
+
+const Hits: React.FC = () => {
+	const { hits } = useHits();
+	return (
+		<div>
+			{hits.map((h: any) => (
+				<div>
+					<h2>{h.author_name}</h2>
+					<img src={h.author_image_url} />
+				</div>
+			))}
+		</div>
+	);
+};
 export default Home;
